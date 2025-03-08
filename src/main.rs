f0f7c11be5e6a12e2be9f3f32304eb832bd2e257
wait-what <25844213+wait-what@users.noreply.github.com>
2025-03-08 08:52:56 +0200

    improve stats

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use macroquad::prelude::*;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 
 fn window_conf() -> Conf {
     Conf {
@@ -53,10 +53,10 @@ async fn main() {
                     start_time = Instant::now();
                     duration = Duration::from_millis(rand::gen_range(1500, 5000));
                     state = State::Waiting;
-                },
+                }
                 State::Waiting => {
                     state = State::Failed;
-                },
+                }
                 State::Go => {
                     state = State::Paused;
                     scores.push(Instant::now().duration_since(start_time + duration));
@@ -66,7 +66,7 @@ async fn main() {
 
         match state {
             State::Paused => {
-                let last_5 = {
+                let last_5 = if scores.len() == 0 { "none".to_string() } else {
                     let mut scores_str = String::new();
 
                     for score in scores.iter().rev().take(5).rev() {
@@ -76,46 +76,58 @@ async fn main() {
                     scores_str
                 };
 
-                let average = {
-                    let sum: u128 = scores.iter().map(|d| d.as_millis()).sum();
+                let last_5_average = {
+                    let sum: u128 = scores.iter().rev().take(5).rev().fold(Duration::from_millis(0), |acc, &x| acc + x).as_millis();
+                    let len = if scores.len() > 5 { 5 } else { scores.len() } as u128;
+
+                    if len == 0 {
+                        "N/A".to_owned()
+                    } else {
+                        (sum / len).to_string() + "ms"
+                    }
+                };
+
+                let total_average = {
+                    let sum: u128 = scores.iter().fold(Duration::from_millis(0), |acc, &x| acc + x).as_millis();
                     let len = scores.len() as u128;
 
                     if len == 0 {
-                        "0".to_owned()
+                        "N/A".to_owned()
                     } else {
-                        (sum / len).to_string()
+                        (sum / len).to_string() + "ms"
                     }
                 };
 
                 clear_background(WHITE);
                 if text_enabled {
                     draw_text(&format!("Click to start"), 20.0, 20.0, 30.0, BLACK);
-                    draw_text(&format!("Last 5: {}", last_5), 20.0, 60.0, 30.0, BLACK);
-                    draw_text(&format!("Total average: {}ms", average), 20.0, 100.0, 30.0, BLACK);
+                    draw_text(&format!("Last 5: {}", last_5), 20.0, 50.0, 30.0, BLACK);
+                    draw_text(&format!("Average of last 5: {}", last_5_average), 20.0, 80.0, 30.0, BLACK);
+                    draw_text(&format!("Total average: {}", total_average), 20.0, 110.0, 30.0, BLACK);
                 }
-            },
+            }
             State::Waiting => {
                 clear_background(BLUE);
                 if text_enabled {
                     draw_text("Wait for green", 20.0, 20.0, 30.0, BLACK);
                 }
-            },
+            }
             State::Go => {
                 clear_background(GREEN);
 
                 if text_enabled {
                     draw_text("Click", 20.0, 20.0, 30.0, BLACK);
                 }
-            },
+            }
             State::Failed => {
                 clear_background(RED);
 
                 if text_enabled {
                     draw_text("Too early", 20.0, 20.0, 30.0, BLACK);
                 }
-            },
+            }
         }
 
         next_frame().await
-    };
+    }
 }
